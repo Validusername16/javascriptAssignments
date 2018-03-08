7acf5a9914b005d36ed38b6ac5b4b0a5cffa3bf6
carlson.cm.s <validusername16@gmail.com>
2018-03-08 15:05:39 -0500

    added basic io stuff

--- public/javascripts/assignment-5.js
+++ public/javascripts/assignment-5.js
@@ -1,3 +1,23 @@
 "use strict";
-const PROMPT = require('readline-sync');
+const PROMPT = require('readline-sync');``
 const IO = require('fs');
+let customers = [];
+
+function main() {
+
+}
+
+function init() {
+
+}
+
+init();
+main();
+
+function load() {
+    let file = IO.readFileSync('data\data.csv','utf-8');
+    let lines= file.toString().split(/\r?\n/); // Automatically creates SD array on newlines
+    for (let i = 0; i < lines.length; i++) {
+        customers.push(lines[i].toString().split(/,/)); // Makes students array MD by pushing data between commas in
+    }
+}

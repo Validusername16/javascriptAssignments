c169b65deec8c0a1451223767064f32ec7141b56
carlson.cm.s <validusername16@gmail.com>
2018-03-12 15:01:43 -0400

    s l o w l y  g e t t i n g  f u r t h e r

--- public/javascripts/assignment-5.js
+++ public/javascripts/assignment-5.js
@@ -4,20 +4,21 @@ const IO = require('fs');
 let customers = [];
 
 function main() {
-
+    console.log(customers[0][1]);
+    coupon
 }
 
 function init() {
+    load();
 
 }
 
 init();
 main();
-
 function load() {
-    let file = IO.readFileSync('data\data.csv','utf-8');
-    let lines= file.toString().split(/\r?\n/); // Automatically creates SD array on newlines
+    let file = IO.readFileSync('data.csv','utf-8');
+    let lines= file.toString().split(/\r?\n/);
     for (let i = 0; i < lines.length; i++) {
-        customers.push(lines[i].toString().split(/,/)); // Makes students array MD by pushing data between commas in
+        customers.push(lines[i].toString().split(/,/));
     }
 }
